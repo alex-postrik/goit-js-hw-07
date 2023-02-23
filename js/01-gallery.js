d6547343f1fc366a6cf01a0832b4d81d87de9dfd
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-02-23 14:28:09 +0100

    add-imgcard

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// =================== search link div-gallery ========================
+
+const galleryContainer = document.querySelector('div.gallery');
+
+// ===================function add div/a/img/-card=====================
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+
+function createGalleryItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>`;
+    })
+    .join("");
+}
+
+// ======================  2v  ========================================
+
+// galleryItems.map(({ preview, original, description }) => {
+//     const div = document.createElement('div')
+//     div.classList.add("gallery__item");
+//     galleryContainer.append(div);
+//     const a = document.createElement('a');
+//     a.href = "large-image.jpg";
+//     a.classList.add("gallery__link");
+//     div.append(a);
+//     const img = document.createElement('img');
+//     img.classList.add("gallery__image");
+//     img.src = preview;
+//     img.setAttribute("data-source", original);
+//     img.alt = description;
+//     a.append(img);
+// });
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+// =================== search link div-gallery ========================
+
+const galleryContainer = document.querySelector("ul.gallery");
+
+// ===================function add div/a/img/-card=====================
+
+galleryItems.map(({ preview, original, description }) => {
+  const div = document.createElement("div");
+  div.classList.add("gallery__item");
+  galleryContainer.append(div);
+  const a = document.createElement("a");
+  a.href = "large-image.jpg";
+  a.classList.add("gallery__link");
+  div.append(a);
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = preview;
+  img.setAttribute("data-source", original);
+  img.alt = description;
+  a.append(img);
+});
